978d1d5ad9d45ce34611d0e1f1747725a6548a01
James Sentinella <james@sentinella.me>
2024-02-02 00:29:02 -0800

    Compile script now outputs a version txt file just like our deploy script. The script also drops the git stash due to toml changes that we don't want to keep. The final step is to tar and zip the file just as the deploy script would.

--- compile.sh
+++ compile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+ARCH=$(uname -m)-linux
 echo "---------------------------------------------------------------------------------"
 echo Getting release version.
 RELEASE_VERSION=$(git describe --tag | sed s/v// | sed -E s/[\.]/\\\\\./g | awk -F \- '{print $1}')
 echo "---------------------------------------------------------------------------------"
 echo Updating version in toml file.
 sed -i -E "0,/^version.*$/{s/^version.*$/version = '${RELEASE_VERSION}'/}" Cargo.toml
+FILE_VERSION=$(git describe --tag | awk -F \- '{print $1}')
+echo $FILE_VERSION > quit-version.txt
 echo "---------------------------------------------------------------------------------"
 echo Building portal software for host architecture.
 echo "---------------------------------------------------------------------------------"
@@ -22,3 +25,9 @@ else
 fi;
 git stash
 git stash drop
+echo Creating tar
+echo "---------------------------------------------------------------------------------"
+tar -cvf ${ARCH}-${FILE_VERSION}.tar -C target/release/ chronokeep-portal-quit ../../quit-version.txt
+rm quit-version.txt
+gzip ${ARCH}-${FILE_VERSION}.tar
+cp ${ARCH}-${FILE_VERSION}.tar.gz ~/
